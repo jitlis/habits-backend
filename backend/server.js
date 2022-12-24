a626381381c41db275840db92b984e841999563a
jitlis <junseok512@gmail.com>
2022-12-23 22:03:12 -0600

    connect to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,31 @@
 require("dotenv").config();
 const express = require("express");
-const app = express();
 const path = require("path");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const corsConfig = require("./config/corsConfig");
+const mongoose = require("mongoose")
 
+const app = express();
+const PORT = process.env.PORT || 3500;
+
+app.use(cors(corsConfig));
 app.use(express.json());
+
+const mongoConnect = async function(){
+try { 
+    await mongoose.connect(process.env.DATABASE_URI) //connect to database
+} catch (err) {
+    console.log(err)
+}
+}
+
+mongoConnect();
+
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB')
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+})
+
 
 
